914c598064bc655eb47f85056ab8965ec71dc4b3
oagomezz <ogomezbarragan437@gmail.com>
2023-10-27 13:37:06 -0700

    added some styling and centered text and customer name boxes for the different reviews.

--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -9,15 +9,27 @@ const Testimonials = () => {
             <div id='text-container'>
                 <h2 id='testimonial-header'>Reviews</h2>
                 <div className='testimonials'>
+                <div className='centered-text'>
                 {testimonials.map((testimonial, index) => {
                     return(
-                        <div className='ind-test-container'>
-                            <p className='testimonial' key={index}>{testimonial.test}</p>
-                            <h2 className='customer'> - {testimonial.customer}</h2>
-                        </div>
-                    )
-                })}
+                        <>
+                            <div  className='test-img-container'key={index}>
+                                <div className='pictures'>
+                                {testimonial.pictures.map((img, index) => {
+                                        {return(
+                                            <img className='picture' key={index} src={img} alt='finished car' />
+                                        )}
+                                    })}
+                                </div>
+                                <div className='ind-test-container'>
+                                    <p className='testimonial' key={index}>{testimonial.test}</p>
+                                    <h2 className='customer'> - {testimonial.customer}</h2>
+                                </div>
+                            </div>
+                        </>
+                    )})}
                 </div>
+            </div>
             </div>
         </div>
     </div>
--- src/logic.js
+++ src/logic.js
@@ -24,19 +24,23 @@ export const pages = [
 export const testimonials = [
     {
     test: "While Creative Stitch Stitchworkz is still fairly new Eros has done a fantastic job making my dream come true creating the canvas top for my custom VW. Any upholstery I may need this is the place I will always go to!! I will always recommend Creative Stitchworkz to anyone!",
-    customer: 'Peter Hangslou'
+    customer: 'Peter Hangslou',
+    pictures: ['https://lh5.googleusercontent.com/p/AF1QipPcNzL62L9x6SJUrFj2OnY_brlWR248VpU63Ycd=w600-h321-p-k-no', "https://lh5.googleusercontent.com/p/AF1QipPSCNIYoIQZIZ1bAQQrbVGq2k4Ba3V5YKbtayI2=w600-h650-p-k-no", "https://lh5.googleusercontent.com/p/AF1QipN0wbnh0ZLvgv3X-Nw8S0AjFUV6uG9O29CebgpV=w600-h650-p-k-no", "https://lh5.googleusercontent.com/p/AF1QipMpMwdue6fIrmEfVKJUh53kzAQLUKk30ZKxoO5J=w600-h650-p-k-no"]
 },
 {
     test: "Great experience, highly recommend. He knocked it out of the park on my seat. Thanks",
-    customer: 'Jesus Ruiz'
+    customer: 'Jesus Ruiz',
+    pictures: [],
 },
 {
     test: "We were very pleased with the service, communication, quality of the product used and the results are wonderful",
-    customer: 'Stacy Evans'
+    customer: 'Stacy Evans',
+    pictures: [],
 },
 {
     test: "Outstanding on seats, headliners and more!!",
-    customer: 'KJ Ulmer'
+    customer: 'KJ Ulmer',
+    pictures: [],
 }
 ]
 
